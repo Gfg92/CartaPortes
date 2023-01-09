93181f166def27b45419b9c28f8f04625d649c55
Guillermo Fernandez <alufer3712@ieselcaminas.org>
2023-01-09 12:07:31 +0100

    Initial commit

--- app/src/main/java/com/example/cartaportes/project/screens/myScreen/SixthScreen.kt
+++ app/src/main/java/com/example/cartaportes/project/screens/myScreen/SixthScreen.kt
@@ -10,14 +10,12 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
