8254ca1319ddb4a8bb8578045415aef35a365633
Guillermo Fernandez <alufer3712@ieselcaminas.org>
2023-01-12 12:24:31 +0100

    App Terminada

--- app/src/main/java/com/example/cartaportes/project/navigation/ScreenNavigation.kt
+++ app/src/main/java/com/example/cartaportes/project/navigation/ScreenNavigation.kt
@@ -13,6 +13,7 @@ import com.example.cartaportes.project.screens.myScreen.*
 @Composable
 fun NavigationScreen(){
     val navController = rememberNavController()
+
     NavHost(navController = navController, startDestination = "login") {
         composable("login") { Login(navController) }
         composable("firstScreen") { FirstScreen(navController) }
