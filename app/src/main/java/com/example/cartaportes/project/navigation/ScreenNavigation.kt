673511a060a5634f4bbe187ac6ac6c6afed467bd
Guillermo Fernandez <alufer3712@ieselcaminas.org>
2023-01-15 15:07:07 +0100

    Creando login

--- app/src/main/java/com/example/cartaportes/project/navigation/ScreenNavigation.kt
+++ app/src/main/java/com/example/cartaportes/project/navigation/ScreenNavigation.kt
@@ -7,6 +7,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.cartaportes.project.screens.login.Login
+import com.example.cartaportes.project.screens.login.Prueba
 import com.example.cartaportes.project.screens.myScreen.*
 
 @RequiresApi(Build.VERSION_CODES.N)
@@ -14,7 +15,8 @@ import com.example.cartaportes.project.screens.myScreen.*
 fun NavigationScreen(){
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "login") {
+    NavHost(navController = navController, startDestination = "prueba") {
+        composable("prueba") { Prueba(navController) }
         composable("login") { Login(navController) }
         composable("firstScreen") { FirstScreen(navController) }
         composable("secondScreen") { SecondScreen(navController) }
